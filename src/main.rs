bc376ec9e186fc10995551b2bbe0cb0996a5c815
mecaneer23 <mecaneer23@gmail.com>
2023-09-25 16:43:37 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::io::{self, Write};
+
+fn pos_int(s: &str) -> i32 {
+    let mut num: i32 = 0;
+    let len: u32 = s.len() as u32;
+    let mut multiplier = 1;
+    for (i, byte) in s.bytes().enumerate() {
+        if byte == 45 {
+            multiplier = -1;
+        } else if byte > 47 && byte < 58 {
+            num += (byte - 48) as i32 * i32::pow(10, len - (i as u32) - 1);
+        }
+    }
+    num * multiplier
+}
+
+fn input(query_string: &str) -> String {
+    print!("{}", query_string);
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    input.trim().to_string()
+}
+
+fn main() {
+    println!("{}", pos_int(&input("Enter a number: ")));
+}
